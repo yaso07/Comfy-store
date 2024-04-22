66f2c184d9921f437b37ee30505e80a6c01e1899
yaso07 <89770165+yaso07@users.noreply.github.com>
2024-04-22 11:23:41 +0530

    changes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,14 @@
+import { useEffect } from 'react';
 import { Filters, PaginationContainer, ProductsContainer } from '../components';
 import { customFetch } from '../utils';
+import axios from 'axios';  
+
 const url = '/products';
 
 const allProductsQuery = (queryParams) => {
   const { search, category, company, sort, price, shipping, page } =
     queryParams;
-
+     
   return {
     queryKey: [
       'products',
@@ -30,16 +33,31 @@ export const loader =
     const params = Object.fromEntries([
       ...new URL(request.url).searchParams.entries(),
     ]);
-
+    
+     const data = async () => {
+       const res = await axios.get("http://localhost:3200/api/products");
+       console.log(res);
+     };
+     data();
     const response = await queryClient.ensureQueryData(
       allProductsQuery(params)
     );
+   
     const products = response.data.data;
+     console.log(products);
     const meta = response.data.meta;
+    console.log(meta)
     return { products, meta, params };
   };
 
 const Products = () => {
+
+      useEffect(()=>{
+         console.log("asdsa")
+           
+
+            
+      })
   return (
     <>
       <Filters />
